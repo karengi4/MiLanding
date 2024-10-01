6cf0b5c88f30f1ee46d73ded08da678e81ea093b
Karen Leon <karengi4.sao@gmail.com>
2024-10-01 17:15:47 -0300

    Enrutamiento

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../../services/firebase/firebaseConfig';
+import { db } from '../../../firebaseConfig';
 import ItemDetail from '../ItemDetail/ItemDetail';
 
 const ItemDetailContainer = () => {
